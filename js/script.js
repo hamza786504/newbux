5e86cb3e02ddf71f1679eb493454408e556fefd4
hamza <hamza786504@gmail.com>
2022-07-28 03:02:04 +0500

    first test of animation

--- js/script.js
+++ js/script.js
@@ -7,3 +7,24 @@ $(function () {
       speed: 40
     });
 });
+
+
+const single_point = document.querySelectorAll(".points > p");
+let i = 0;
+window.addEventListener("load", function(){
+  setInterval(() => {
+    if(i < single_point.length){
+      single_point[i + 1].classList.remove("d-none");
+      // return;
+    }
+    i++;
+  }, 2000);
+})
+
+
+
+// window.onload = function(){
+//   setInterval(function(){
+//     console.log("Hello");
+//   }, 1000);
+//  };
